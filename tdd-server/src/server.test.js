46f1a8cffcf1f6afd960c9f684d92e82f6b62c89
vasudevapitta <tizzicboy@gmail.com>
2020-06-20 22:03:43 -0500

    Implementing a server endpoint

--- tdd-server/src/server.test.js
+++ tdd-server/src/server.test.js
@@ -1,7 +1,8 @@
-import sinon from 'sinon';
+import sinon, { fake } from 'sinon';
 import request from 'supertest';
 import expect from 'chai';
 import db from './db';
+import { app } from './server';
 
 describe   ('GET /users/:username', ()=>{
     it('sends the correct response when a user with the username is found', async ()=>{
@@ -13,6 +14,13 @@ describe   ('GET /users/:username', ()=>{
 
         const stub = sinon.stub(db, 'getUserByUsername').resolves(fakeData);
         //The way sinon works is by actually replacing the getUserByUsername method of our database wrapper with a fake version
+
+        await request(app).get('/users/abc')
+        .expect(200) //status code
+        .expect('Content-Type', /json/) //content type header
+        .expect(fakeData); //response body
+
+        expect(stub.getCall(0).args[0]).to.equal('abc');
 
         stub.restore;
         //And calling this stub.restore here makes sure that the method is returned to normal before any other tests run
